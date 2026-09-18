296a61a5773440b21efd8c83fe3f5ee92695a009
g-mmorgan <117980931+g-mmorgan@users.noreply.github.com>
2026-09-18 07:05:28 +0000

    Primer commit

---
+++ hola-mundo.py
@@ -0,0 +1,8 @@
+print ("Hola mundo")
+
+#Los comentarios van con # no //
+
+if 2>1:
+ print("En python es muy importante la indentacion")
+
+ print("Print es una sentencia"); print("Puedes apilar sentencias así")
